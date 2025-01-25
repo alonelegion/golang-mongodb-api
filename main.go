008372aa9d49900af11c1592447f368f1451a866
alonelegion <berdin.andrey2@wb.ru>
2025-01-26 01:17:55 +0300

    sort import

--- main.go
+++ main.go
@@ -6,12 +6,13 @@ import (
 	"log"
 	"net/http"
 
-	"github.com/alonelegion/golang-mongodb-api/config"
 	"github.com/gin-gonic/gin"
 	"github.com/go-redis/redis/v8"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 	"go.mongodb.org/mongo-driver/mongo/readpref"
+
+	"github.com/alonelegion/golang-mongodb-api/config"
 )
 
 var (
