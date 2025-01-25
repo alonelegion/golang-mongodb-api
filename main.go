ccfb3064d90a076c85c4e3341a404e67d628839a
Berdin <andrey.berdin@sbermarket.ru>
2025-01-26 01:08:48 +0300

    add main file

--- main.go
+++ main.go
@@ -2,10 +2,16 @@ package main
 
 import (
 	"context"
+	"fmt"
+	"log"
+	"net/http"
 
+	"github.com/alonelegion/golang-mongodb-api/config"
 	"github.com/gin-gonic/gin"
 	"github.com/go-redis/redis/v8"
 	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
 )
 
 var (
@@ -14,3 +20,64 @@ var (
 	mongoclient *mongo.Client
 	redisclient *redis.Client
 )
+
+func init() {
+	config, err := config.LoadConfig(".")
+	if err != nil {
+		log.Fatal("Could not load environment variables", err)
+	}
+
+	ctx = context.TODO()
+
+	mongoconn := options.Client().ApplyURI(config.DBUri)
+	mongoclient, err := mongo.Connect(ctx, mongoconn)
+	if err != nil {
+		panic(err)
+	}
+
+	if err = mongoclient.Ping(ctx, readpref.Primary()); err != nil {
+		panic(err)
+	}
+
+	fmt.Println("MongoDB successfully connected...")
+
+	redisclient = redis.NewClient(&redis.Options{
+		Addr: config.RedisUri,
+	})
+	if _, err = redisclient.Ping(ctx).Result(); err != nil {
+		panic(err)
+	}
+
+	err = redisclient.Set(ctx, "test", "Welcome to Golang with Redis and MongoDB",
+		0).Err()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Redis client successfully connected...")
+
+	server = gin.Default()
+}
+
+func main() {
+	config, err := config.LoadConfig(".")
+	if err != nil {
+		log.Fatal("Could not load environment variables", err)
+	}
+
+	defer mongoclient.Disconnect(ctx)
+
+	value, err := redisclient.Get(ctx, "test").Result()
+	if err == redis.Nil {
+		fmt.Println("key: test does not exist")
+	} else if err != nil {
+		panic(err)
+	}
+
+	router := server.Group("/api")
+	router.GET("/healthchecker", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{"status": "success", "message": value})
+	})
+
+	log.Fatal(server.Run(":" + config.Port))
+}
